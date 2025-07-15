9675352695c0959864a493d38a877b0582c814c5
AbdalrhmanAlarbi <abdalrhmanalarbi5@gmail.com>
2025-07-15 10:03:29 +0200

    Profile.js

--- JavaScript/Profile.js
+++ JavaScript/Profile.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const nameElement = document.getElementById("user-name");
+    const storedName = localStorage.getItem("username");
+
+    if (storedName) {
+        nameElement.textContent = storedName;
+       // localStorage.removeItem("username"); // Show once
+    } else {
+        nameElement.textContent = "مجتبى";
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     function showMessage(msg) {
         // Use alert for simplicity; replace with a custom modal if desired
